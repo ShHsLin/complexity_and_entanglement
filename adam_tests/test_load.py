79a9575645b07dae3d5afb58abaca6986c957d97
as2457 <adam.smith.home@ntlworld.com>
2021-12-07 17:37:03 +0000

    Added geometric entanglement and MI

--- adam_tests/test_load.py
+++ adam_tests/test_load.py
@@ -12,7 +12,7 @@ from matplotlib import pyplot as plt
 L = 20
 g = 1.4
 h = 0.9045
-depth = 3
+depth = 8
 N_iter = 5000
 
 save_filename = f"Ising_GS_L{L}_g{g}_h{h}".replace(".","-")
@@ -25,13 +25,33 @@ entropy_list = [entropy_exact]
 
 data = load_obj("HPC_data/circuits/"+save_filename+f"_depth{depth}_N_iter{N_iter}_circuit")
 
-plt.plot(np.log(data["errors"]))
-plt.show()
+#plt.plot(np.log(data["errors"]))
+#plt.show()
+
 
 psi_circuit = State.vector(data["state"].reshape((-1,1)))
+
+
+print(psi.geometricEntanglement_svd())
+print(psi_circuit.geometricEntanglement_svd())
+#print(psi_circuit.geometricEntanglement())
+
 entropy_list.append([psi_circuit.bipartiteEE([x for x in range(y)]) for y in range(L+1) ])
 
 for entropy in entropy_list:
     plt.plot(entropy)
 
+plt.show()
+
+
+
+
+for kk in range(1,10):
+    qubits = [(kk-1)/2 + x for x in range(L-kk)]
+    MI_exact = np.array([psi.mutualInformation([y],[y+kk]) for y in range(L-kk)])
+    #plt.plot(MI)
+    MI = np.array([psi_circuit.mutualInformation([y],[y+kk]) for y in range(L-kk)])
+    plt.plot(qubits,MI/MI_exact)
+
+plt.legend([str(x) for x in range(9)])
 plt.show()
